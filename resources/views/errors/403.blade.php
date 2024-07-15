dd08dfdc759e6d0ad8a3223d0c374f6e6d27b686
coderun23-cloud <natnaelgetachew814@gmail.com>
2024-07-15 15:08:41 +0300

    note-app

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h1 style="color: white;font-size:30px;text-align:center;">Forbidden</h1>
+</x-app-layout>
---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <div class="note-container py-40">
+        <a href="{{ route('note.create') }}" class="new-note-btn">
+            New Note
+        </a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{ Str::words($note->note, 24) }}
+                    </div>
+                    <div class="note-buttons">
+                        <a href="{{ route('note.show', $note) }}" class="note-edit-button">View</a>
+                        <a href="{{ route('note.edit', $note) }}" class="note-edit-button">Edit</a>
+                        <form action="{{ route('note.destroy', $note) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="note-delete-button">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+     <div class="p-y=16">
+        {{$notes->links()}}
+     </div>
+    </div>
+</x-app-layout>
